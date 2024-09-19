f202247569a056a847b54c1d974296fdd4498df1
bsalunga-cen <bsalunga@my.centennialcollege.ca>
2024-09-19 11:27:04 -0400

    updated vite config

--- components/MainRouter.jsx
+++ components/MainRouter.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import About from '../src/about'
-import Contact from '../src/contact'
-import Services from '../src/services'
-import Project from '../src/project'
-import Layout from './Layout'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from '../src/about';
+import Contact from '../src/contact';
+import Services from '../src/services';
+import Project from '../src/project';
+import Layout from './Layout';
 
 const MainRouter = () => {
     return (<div>
         <Layout/>
         <Routes>
-
             <Route exact path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
             <Route exact path="/services" element={<Services />} />
